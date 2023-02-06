a040f8159bbc9ef3c24881a275a21986b80b8735
darkmoster@naver.com <darkmoster@naver.com>
2023-02-06 20:28:09 +0900

    0206

--- 03_loop/src/multi_/multifor4.java
+++ 03_loop/src/multi_/multifor4.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class multifor4 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		
+		for(int i=2; i<10; i++) {
+			for(int j=1; j<10; j++) {
+				System.out.print(i+"*"+j+"="+i*j+" ");
+			}
+			System.out.println("");
+		}
 		
 
 	}
