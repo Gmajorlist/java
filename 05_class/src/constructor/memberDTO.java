ecc9c2bf3f8e0d2ed1b6d590bc4ff5b84de626f8
darkmoster@naver.com <darkmoster@naver.com>
2023-02-09 16:06:58 +0900

    0208

--- 05_class/src/constructor/memberDTO.java
+++ 05_class/src/constructor/memberDTO.java
@@ -8,9 +8,24 @@ public class memberDTO {// 1인분클래스// DTo = Data transfer Object
 	private String address;
 	
 	//생성자를 통해서 데이터 4개를 받아본다.
-	
-		
-
+	public memberDTO(String name, int age, String phone,String address ) {
+		this.name = name;
+		this.age = age;
+		this.phone =phone;
+		this.address =address;
+	}
+		public String getName() {
+			return name;
+		}
+		public int getAge() {
+			return age;
+		}
+		public String getPhone() {
+			return phone;
+		}
+		public String getAddress() {
+			return address;
+		}
 	}
 
 
--- 05_class/src/constructor/memberservice.java
+++ 05_class/src/constructor/memberservice.java
@@ -3,6 +3,7 @@ package constructor;
 import java.util.Scanner;
 
 public class memberservice {
+	Scanner sc = new Scanner(System.in);
 	private memberDTO[] ar = new memberDTO[5];// 객체 배열
 	
 	public memberservice() {
@@ -10,7 +11,7 @@ public class memberservice {
 		
 	}
 	public void menu() {
-		Scanner sc = new Scanner(System.in);
+	
 		int num;
 		
 		while(true) {
@@ -25,17 +26,71 @@ public class memberservice {
 			num = sc.nextInt();
 			
 			if(num == 5)break; //while 벗어나라
-			if(num == 1)insertArticle();
+			if(num == 1)insert();
 			else if(num == 2 )list();
+			else if(num == 3 )update();
 		}//while
 }	// menu
-	public void list() {
+	public void update() {
+		System.out.println("핸드폰 번호 입력 : ");
+		String phone = sc.next();
 		
+		int i;
+		for( i=0; i<ar.length; i++) {
+			if(ar[1] != null) {
+				if(ar[i].getPhone().equals(phone)) {
+					System.out.println(ar[i].getName()+ "\t"
+							+ar[i].getAge()+ "\t"
+							+ar[i].getPhone()+ "\t"
+							+ar[i].getAddress());
+					break;
+				}
+			}
+		}//for i
+		if(i == ar.length) System.out.println("찾는 회원이 없습니다.");
 		
 	}
+	public void list() {
+		//for(int i=0; i<ar.length; i++) 
+		for(memberDTO dto : ar) {
+			if(dto !=null) {
+				System.out.println(dto.getName()+ "\t"
+						+dto.getAge()+ "\t"
+						+dto.getPhone()+ "\t"
+						+dto.getAddress());
+			}
+		}
+	}
 
-	public void insertArticle() {
+	public void insert() {
+		int i ;
+		for( i =0; i<ar.length; i++) {
+			if(ar[i] == null) break;
+		}
+		if( i == ar.length) {
+			System.out.println(ar.length +"명의 정원이 꽉 찼습니다.");
+			return; //함수를 벗어나라
+		}
+		//데이터 입력
+		System.out.println(" 이름 입력 : " );
+		String name = sc.next();
+		System.out.println("나이 입력 :");
+		int age = sc.nextInt();
+		System.out.println("핸드폰 입력 : ");
+		String phone = sc.next();
+		System.out.println("주소 입력");
+		String address = sc.next();
+		
+		//memberDTO 클래스 생성
+		for( i =0; i<ar.length; i++) {
+			if(ar[i] ==null) {
+				ar[i]= new memberDTO(name,age,phone,address);
+				break; //for 벗어나라
+			}
+		}
 		
 		
+		
+		System.out.println("1 row created");
 	}
 }
