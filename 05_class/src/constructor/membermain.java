cf0c2f4e782a9532c913e4f543b1c2ec9d623aed
darkmoster@naver.com <darkmoster@naver.com>
2023-02-10 10:35:51 +0900

    0210

--- 05_class/src/constructor/membermain.java
+++ 05_class/src/constructor/membermain.java
@@ -8,6 +8,9 @@ public class membermain {
 		System.out.println("프로그램을 종료합니다.");
 		// 항상 일하는 집합체한테 일을 넘김
 		//간단하게 함 서비스가 일을 다함
+		//static < 시음식이라고 생각하면됨 굳이 new 필요가 없음
+		// static 밑에 private 들어가면 new를 해줘야함 
+		//
 		
 	}
 
