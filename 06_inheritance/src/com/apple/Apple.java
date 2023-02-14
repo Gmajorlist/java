963f6367ff3f1f0a72801084d513cd986d892f04
darkmoster@naver.com <darkmoster@naver.com>
2023-02-14 10:41:15 +0900

    0214

--- 06_inheritance/src/com/apple/Apple.java
+++ 06_inheritance/src/com/apple/Apple.java
@@ -1,10 +1,21 @@
 package com.apple;
+import com.zoo.Zoo;
 
 public class Apple {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public static void main(String[] args) {
+		System.out.println("빨간 사과");
+		
+		
+		//zoo클래스 tiger () 호출
+		//1.동물원 클래스 생성 위에 임포트 해줘
+		Zoo z = new Zoo();
+		z.tiger();
+//		z.giraffe(); // 외부에선 가져올수없음 protected
+//		z.elephant(); //
+//		z.lion(); 
+		
 	}
 
 }
---
+++ 06_inheritance/src/com/zoo/Zoo.java
@@ -0,0 +1,26 @@
+package com.zoo;
+
+public class Zoo {
+	public void tiger() {
+		System.out.println("무서운 호랑이");
+	}
+	protected void giraffe() { //외부에선 가져올수없어 protected
+		System.out.println("목이 긴 기린");
+	}
+	void elephant() {
+		System.out.println("뚱뚱한 코끼리");
+	}
+	private void lion() {
+		System.out.println("멋진 사자");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
